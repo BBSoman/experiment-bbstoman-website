c7def9baf3e6f25837de3d37582a9829003d2ef2
BBSoman <harshumeshshah23@hotmail.com>
2026-08-20 11:09:46 +0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
+import PartnerMarquee from '../components/PartnerMarquee';
 import BrandPartners from '../components/BrandPartners';
 import Footer from '../components/Footer';
 
@@ -11,6 +12,7 @@ const HomePage: React.FC = () => {
       <Header />
       <Hero />
       <Services />
+      <PartnerMarquee />
       <BrandPartners />
       <Footer />
     </>
